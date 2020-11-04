36e22642cba4ace390f785a9361cb44d1e9c22c2
Jeff <JeffXJin@gmail.com>
2020-11-04 13:25:59 -0800

    added settings.html,.css.js

---
+++ scripts/settings.js
@@ -0,0 +1,28 @@
+/*<![CDATA[*/
+    $(document).ready(function() {
+
+        var settings = ["Distance Units", "Map History", "setting 3?", "setting 4?", "idk"];
+
+        for (var i = 4; i >= 0; i--) {
+            
+            let section = "<h5 class=\"section\">" + settings[i] + "</h5>";
+            
+            let toggle = 
+                "<div class=\"toggle\" data-toggle=\"buttons\">" +
+                    "<label class=\"btn btn-secondary active\">" +
+                    //on
+                    "<input type=\"radio\" name=\"options\" id=\"option1\" checked> On </label>" + 
+                    "<label class=\"btn btn-secondary\">" + 
+                    //off
+                    "<input type=\"radio\" name=\"options\" id=\"option2\"> Off </label>" + 
+                "</div>";
+            $(".settingsSection")
+            .after(
+                $('<div class="board"></div>')
+                    .append('<link rel="stylesheet" href="settings.css" type="text/css" />')
+                    .append(section)
+                    .prepend(toggle)
+            );
+        }
+        //$(".toggle").button('toggle');
+    });
