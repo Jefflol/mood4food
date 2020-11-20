ca204b05046bfa6f8742a60be911c56bab457aac
Sunmin Choi <sunminchoi1214@gmail.com>
2020-11-20 00:27:46 -0800

    Added ability to get and show user's name from database

---
+++ scripts/display_user.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  var userNameToShow;
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      //sets userName
+      userNameToShow = user.displayName;
+
+      //Displays all userNames in document, must be called from onAuthStateChanged to work because of asynchronous
+      documentDisplayName();
+
+    } else {
+      console.log("User logged on = false");
+    }
+  });
+
+    //FUNCTION THAT SETS ALL INSTANCES OF CLASS TO THE CURRENT USERNAME
+    function documentDisplayName(){
+      var x = document.getElementsByClassName("user_name");
+        var i;
+        for (i = 0; i < x.length; i++) {
+          x[i].innerHTML = userNameToShow;
+        }
+      var y = document.getElementsByClassName("mouse-over-username");
+        var num;
+        for (num = 0; num < y.length; y++) {
+          y[num].setAttribute("title", "Logged in as: " + userNameToShow)
+        }
+      console.log("Updated username");
+    }
+});
