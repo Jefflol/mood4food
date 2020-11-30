51fc37026e41df24d9d03bd2b718d202b3afb115
Jeffrey Chen <jeffrey0.0chen@gmail.com>
2020-11-30 12:25:16 -0800

    Add back button to navbar for consistency

--- scripts/restaurant-details.js
+++ scripts/restaurant-details.js
@@ -122,19 +122,7 @@ const displayRestaurantDetails = (restaurantObj) => {
     avgThumbs
   } = restaurantObj;
 
-  let restaurantDetails = $(`
-    <!-- Back Button -->
-    <a id="restaurantDetailsBackBtn" type="button" class="back-btn btn btn-light" href="restaurant.html">
-        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-left mx-1" fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd"
-            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
-        </svg>
-        <div class="mx-1">
-            Go Back
-        </div>
-    </a>
-    
+  let restaurantDetails = $(`    
     <!-- Restaurant Header - Img + Brief Details -->
     <div class="restaurant__header">
       <div class="restaurant__img">
